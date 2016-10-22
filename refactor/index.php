e9f516ddf2dc6906d35b64d1c9c8f72272dc3180
Samantha Klein <saradurga@gmail.com>
2016-10-22 14:12:32 -0500

    placeholder for refactor

---
+++ refactor/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Trello.print()</title>
+</head>
+<body>
+  <div class="row header div1">
+    <h1>Trello.print()</h1>
+  </div>
+  <div class="row div2">
+    <div class="col-md-3" id="boards">
+      <h3>Your Boards</h3>
+    </div>
+    <div class="col-md-9 div3" id="checklists">
+    </div>
+  </div>
+</body>
+</html>
